666bdd8a1316f7947a9bd284aa897a76b2885c2d
md-mridul-ali <mridulaiub17@gmail.com>
2026-02-15 23:16:01 +0600

    design word container and some tasks are under processing

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,27 @@ const loadLessons = () => {
         .then((response) => response.json())
         .then((jsonData) => displayLessons(jsonData.data));
 };
+//fetch word by level
+const loadLevelWord = (id) =>{
+        fetch(`https://openapi.programming-hero.com/api/level/${id}`)
+        .then((response) => response.json())
+        .then((data) => displayLevelWord(data.data));
 
+};
+const displayLevelWord = (words) =>{
+    const wordContainer = document.getElementById("word-container");
+    wordContainer.innerHTML = "";
+     words.forEach(word => {
+        const card = document.createElement("div");
+        card.innerHTML = `
+         <p>Cat</P>
+
+        `;
+
+        wordContainer.append(card);
+     });
+}
+//fetch all levels
 const displayLessons = (lessons) => {
     const levelContainer = document.getElementById('level-container');
     levelContainer.innerHTML = "";
@@ -13,7 +33,7 @@ const displayLessons = (lessons) => {
     for (let lesson of lessons) {
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
-          <button class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open-reader"></i>
+          <button onclick = "loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open-reader"></i>
                                 Lesson -${lesson.level_no}</button>
         `;
 
