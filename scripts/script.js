98d09d727a743d3b425a00276f6e05b43ac450d4
md-mridul-ali <mridulaiub17@gmail.com>
2026-02-16 18:52:32 +0600

    fetch all words

--- scripts/script.js
+++ scripts/script.js
@@ -15,15 +15,34 @@ const loadLevelWord = (id) =>{
 const displayLevelWord = (words) =>{
     const wordContainer = document.getElementById("word-container");
     wordContainer.innerHTML = "";
-     words.forEach(word => {
+     if(words == ""){
+          wordContainer.innerHTML = `
+             <div class="font-bangla text-center col-span-full py-10 rounded-xl space-y-6">
+              <img class="mx-auto" src="./assets/alert-error.png" alt="Error">
+            <p class="text-xl font-medium text-gray-400">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+            <h2 class="font-bold text-3xl">নেক্সট Lesson এ যান</h2>
+          </div>
+          `
+     }
+     else{
+        words.forEach(word => {
         const card = document.createElement("div");
         card.innerHTML = `
-         <p>Cat</P>
+         <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+            <h2 class="font-bold text-2xl">${word.word? word.word : "Word npt found"}</h2>
+            <p class="font-semibold">Meaning /Pronunciation</p>
+            <div class="font-medium text-2xl font-bangla">"${word.meaning? word.meaning : "Meaning not found"} / ${word.pronunciation? word.pronunciation : "Pronunciation not found"}"</div>
+            <div class="flex justify-between items-center">
+                 <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+                 <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+            </div>
+          </div>
 
         `;
 
         wordContainer.append(card);
      });
+     }
 }
 //fetch all levels
 const displayLessons = (lessons) => {
